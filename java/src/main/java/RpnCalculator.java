7cf89c92317305395f35c05fe62ee6cf20ea719c
zuzannaweichel-accenture <zuzanna.weichel@accenture.com>
2019-04-10 17:06:03 -0400

    added ability to add two numbers

--- java/src/main/java/RpnCalculator.java
+++ java/src/main/java/RpnCalculator.java
@@ -1,2 +1,13 @@
 public class RpnCalculator {
+    public int calculate(String expression) {
+        String[] elements = expression.split(" ");
+        if(elements.length > 1){
+            int e1 = Integer.parseInt(elements[0]);
+            int e2 = Integer.parseInt(elements[1]);
+            if(elements[2].equals("+")){
+                return e1 + e1;
+            }
+        }
+        return Integer.parseInt(expression);
+    }
 }
--- java/src/test/java/RpnCalculatorTest.java
+++ java/src/test/java/RpnCalculatorTest.java
@@ -4,7 +4,15 @@ import org.junit.Test;
 
 public class RpnCalculatorTest {
 	@Test
-    public void something() {
-        assertEquals(true, false);
+    public void givenOne_return1() {
+	    RpnCalculator calc = new RpnCalculator();
+        String expression = "1";
+        assertEquals(1, calc.calculate(expression));
+    }
+    @Test
+    public void onePlusOne_makes2(){
+        RpnCalculator calc = new RpnCalculator();
+        String expression = "1 1 +";
+        assertEquals(2, calc.calculate(expression));
     }
 }
