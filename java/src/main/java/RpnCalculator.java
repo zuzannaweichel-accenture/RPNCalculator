c34c717dd088b9a5da6f4d364478f6cb1b96c4e6
zuzannaweichel-accenture <zuzanna.weichel@accenture.com>
2019-04-15 09:10:35 -0400

    first go at the calculator with Hillary

--- java/src/main/java/RpnCalculator.java
+++ java/src/main/java/RpnCalculator.java
@@ -1,13 +1,56 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class RpnCalculator {
+
     public int calculate(String expression) {
-        String[] elements = expression.split(" ");
-        if(elements.length > 1){
-            int e1 = Integer.parseInt(elements[0]);
-            int e2 = Integer.parseInt(elements[1]);
-            if(elements[2].equals("+")){
-                return e1 + e1;
+        List<Character> operators = Arrays.asList('+', '-','/', '*');
+       String[] expressionArr = expression.split(" ");
+        while(expressionArr.length > 2){
+            String[] chunks = chunkify(expression,operators);
+            int calculatedBase = calculateBase(chunks[1]);
+
+            StringBuilder dechunk = new StringBuilder();
+            String reducedExpression = dechunk.append(chunks[0]+" ").append(calculatedBase+" ").append(chunks[2]+" ").toString();
+            expressionArr = reducedExpression.split(" ");
+        }
+
+        return Integer.parseInt(expressionArr[0]);
+    }
+
+    private Integer calculateBase(String chunk) {
+        String[] elem = chunk.split(" ");
+        int e1 = Integer.parseInt(elem[0]);
+        int e2 = Integer.parseInt(elem[1]);
+        String op = elem[2];
+        if (op.equals("+")) {
+            return e1 + e2;
+        }
+        if (op.equals("-")) {
+            return e1 - e2;
+        }
+        if (op.equals("*")) {
+            return e1 * e2;
+        }
+        if (op.equals("/")) {
+            return e1 / e2;
+        }
+        return null;
+    }
+
+    private String[] chunkify(String expression, List<Character> operators){
+        int index = 0;
+        for (int i = 0; i <expression.length() ; i++) {
+            if (operators.contains(expression.charAt(i))){
+                index = i;
             }
         }
-        return Integer.parseInt(expression);
+        String prechunk = expression.substring(0, index-4);
+        String chunk = expression.substring(index-4, index+1);
+        String postchunk = expression.substring(index+2);
+
+        return new String[] {prechunk, chunk, postchunk};
     }
+
 }
+
--- java/src/test/java/RpnCalculatorTest.java
+++ java/src/test/java/RpnCalculatorTest.java
@@ -1,18 +1,40 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class RpnCalculatorTest {
-	@Test
-    public void givenOne_return1() {
-	    RpnCalculator calc = new RpnCalculator();
-        String expression = "1";
-        assertEquals(1, calc.calculate(expression));
+	private RpnCalculator calc;
+
+	@Before
+    public void setup(){
+	    calc = new RpnCalculator();
     }
+
     @Test
     public void onePlusOne_makes2(){
-        RpnCalculator calc = new RpnCalculator();
         String expression = "1 1 +";
         assertEquals(2, calc.calculate(expression));
     }
+//    @Test
+//    public void oneMinusOne_makes0(){
+//	    RpnCalculator calc = new RpnCalculator();
+//	    String expression = "1 1 -";
+//	    assertEquals(0, calc.calculate(expression));
+//    }
+//    @Test
+//    public void twoTimesThree_makes6(){
+//        String expression = "2 3 *";
+//        assertEquals(6, calc.calculate(expression));
+//    }
+//    @Test
+//    public void tenDividedByFive_makes2(){
+//	    String expression ="10 5 /";
+//	    assertEquals(2, calc.calculate(expression));
+//    }
+//    @Test
+//    public void fourPlusTwoMinusThree_makes3() {
+//        String expression = "4 2 + 3 -";
+//        assertEquals(3, calc.calculate(expression));
+//    }
 }
